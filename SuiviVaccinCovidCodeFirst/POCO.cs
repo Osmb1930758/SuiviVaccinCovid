e6c597f316a7a532612a794f76e13f37c7bf807d
Billy Osmont <1930758@etu.cegepjonquiere.ca>
2021-01-26 15:06:09 -0500

    test3

---
+++ SuiviVaccinCovidCodeFirst/POCO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuiviVaccinCovidCodeFirst
+{
+    class POCO
+    {
+        public int VaccinId { get; set; }
+        public DateTime Date { get; set; }
+        public string Nom { get; set; }
+        public string NAMPatient { get; set; }
+        public override string ToString()
+        {
+            return $" Vaccin #{VaccinId} ({Nom}), adiminstré le {Date} à { NAMPatient} ";
+        }
+    }
+}
---
+++ SuiviVaccinCovidCodeFirst/VaccinContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace SuiviVaccinCovidCodeFirst
+{
+    public class VaccinContext : DbContext
+    {
+        public DbSet<Vaccin> Vaccins { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        =>
+       options.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=VaccinBD;Trusted_Connection = True;");
+ }
+
+}
