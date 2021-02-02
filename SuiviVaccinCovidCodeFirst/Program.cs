52a8d3fade593fd40f5a78c212b2572b95632d84
osmbi1930758 <osmbi1930758@etu.cegepjonquiere.ca>
2021-02-02 10:24:39 -0500

    test7

--- SuiviVaccinCovidCodeFirst/Program.cs
+++ SuiviVaccinCovidCodeFirst/Program.cs
@@ -6,8 +6,27 @@ namespace SuiviVaccinCovidCodeFirst
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello !");
+            Vaccin dose1Mylene = new Vaccin
+            {
+                Date = DateTime.Today,
+                NAMPatient = "LAPM12345678",
+                Nom = "Moderna"
+            };
+            Vaccin dose1Gaston = new Vaccin
+            {
+                Date = new DateTime(2021, 01, 15),
+                NAMPatient = "BHEG12345678",
+                Nom = "Pfizer"
+            };
+            VaccinContext context = new VaccinContext();
+            context.Vaccins.Add(dose1Mylene);
+            context.Vaccins.Add(dose1Gaston);
+            context.SaveChanges();
+            context.Remove(dose1Gaston);
+            dose1Mylene.Nom = "Pfizer";
+            context.SaveChanges();
+            foreach (Vaccin vaccin in context.Vaccins)
+                Console.WriteLine(vaccin);
         }
     }
 }
